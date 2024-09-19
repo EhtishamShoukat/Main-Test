585ec431b21006e43579b7b4a186f418b87ea593
Ehtisham Shoukat <152707444+EhtishamShoukat@users.noreply.github.com>
2024-09-19 15:54:39 +0500

    Add files via upload

---
+++ Test/User/User-page/user-page.js
@@ -0,0 +1,7 @@
+function addPerson() {
+   document.getElementById("totalPeople").innerText = totalPeople;
+    document.getElementById("totalIncome").innerText = totalIncome.toFixed(2);
+    document.getElementById("totalExpense").innerText = totalExpense.toFixed(2);
+    document.getElementById("totalBalance").innerText = totalBalance.toFixed(2);
+  }
+  
